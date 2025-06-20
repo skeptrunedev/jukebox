15ff8e8832303a3b29a71db6d1c0d9f94a01fb5a
Nick K <nick.k@trieve.ai>
2025-06-20 01:07:06 -0700

    feat: finished working docker setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import runtimeEnv from "vite-plugin-runtime-env";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), runtimeEnv()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
